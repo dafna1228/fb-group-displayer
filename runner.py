af6bf908140f5b62eb18b3cf16c22be23b5a9169
dafna <dafna1228@gmail.com>
2017-04-03 18:08:22 +0300

    finished creating all the desiered docs

--- runner.py
+++ runner.py
@@ -66,6 +66,36 @@ def post_info(post):
 
 
 """
+create a document containing comment details- the poster, id, time created, id, message, and dict of reactions
+"""
+
+def comment_info(comment):
+    comment_doc = {}
+    comment_doc["created_time"] = comment["created_time"]
+    comment_doc["from"] = comment["from"]["name"]
+    comment_doc["id"] = comment["id"]
+    comment_doc["message"] = comment["message"]
+    comment_doc["comment_count"] = comment["comment_count"]
+    comment_doc["like_count"] = comment["like_count"]
+
+    return comment_doc
+
+"""
+create a document containing comment details- the poster, id, time created, id, message, and dict of reactions
+"""
+
+def reply_info(reply):
+    reply_doc = {}
+    reply_doc["created_time"] = reply["created_time"]
+    reply_doc["from"] = reply["from"]["name"]
+    reply_doc["id"] = reply["id"]
+    reply_doc["message"] = reply["message"]
+    reply_doc["parent_id"] = reply['parent']["id"]
+    reply_doc["like_count"] = reply["like_count"]
+
+    return reply_doc
+
+"""
 create a document containing the group name, description, number of members requests
 TODO: get member count from the web, as well as admins
 """
@@ -85,27 +115,31 @@ if __name__ == '__main__':
 
     feed = graph.get_object(id=GID,
                             fields='name, description, member_request_count,'
-                                     'feed.limit(3){created_time, from, reactions, message}')
+                                     'feed.limit(5){created_time, from, reactions, message}')
+ #   print "printing whole feed:"
+ #   pp.pprint(feed)
 
-
-    pp.pprint(feed)
     all_posts = feed['feed']['data']
+    print "\n group description:"
     pp.pprint(group_info(feed))
     for post in all_posts:
         post_doc = post_info(post)
+        print "\n printing post in feed"
         pp.pprint(post_doc)
         comments = graph.get_connections(id=post_doc["id"],
                                              connection_name='comments')
-        print "printing post comments info for post:"
-        #pp.pprint(comments)
         for comment in comments["data"]:
-            comment_info = graph.get_object(id=comment["id"], fields='id, comment_count, from, like_count, '
+            comment_object = graph.get_object(id=comment["id"], fields='id, comment_count, from, like_count, '
                                                                      'created_time, message, parent')
-            pp.pprint(comment_info)
-            print "printing comment replies:"
-            replies_info = graph.get_object(id=comment["id"], fields='id, comment_count, from, like_count, '
-                                                                         'created_time, message, parent')
-            pp.pprint(replies_info)
+            print "\n printing post comments info for post " + post_doc["id"]
+            pp.pprint(comment_info(comment_object))
+            if (comment_info(comment_object)["comment_count"] != 0):
+                print "\n trying to print comment replies:"
+                replies_info = graph.get_object(id=comment["id"], fields='comments{comments, from, id, message, like_count, parent, created_time}')
+                #pp.pprint(replies_info)
+                for reply in replies_info["comments"]["data"]:
+                    reply_doc = reply_info(reply)
+                    pp.pprint(reply_doc)
 
 """
 def get_members(gid):
