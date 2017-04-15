9ae4d9dcbc3fdd4fce35c765ceb416a4d372cf51
dafna <dafna1228@gmail.com>
2017-04-15 21:46:48 +0300

    collector script finished

--- collector.py
+++ collector.py
@@ -8,9 +8,7 @@ import time
 this collects facebook posts, comments, and comment replies in a spesific date range
 
 TODO:
-- get number of members- too hard and gross to write! :(
-
-- fixe time pased cursor- convert date to epoc and shit
+- get number of members- gross to write! :(
 
 - more cleanup
 
@@ -24,16 +22,14 @@ TOKEN = f.readline()
 GID = f.readline()
 graph = facebook.GraphAPI(TOKEN)
 pp = pprint.PrettyPrinter(indent=4)
-SINCE = "1/4/2017"
-UNTIL = "2/4/2017"
+
 
 """
 gets - ['feed']['data']["reactions"]["data"]
 returns a list of dicts, containing a user name and their reaction
 """
 
-
-def get_reactions(reacts):
+def parse_reactions(reacts):
     all_reacts = []
     for i in reacts:
         r = {
@@ -43,14 +39,15 @@ def get_reactions(reacts):
         all_reacts.append(r)
     return all_reacts
 
-
 # parses the post id from an id given from the feed
 def get_post_id(pid):
     return pid.split('_')[1]
 
 
 """
-create a document containing the poster, id, time created, id, message, and dict of reactions
+create a document containing the post info-  the user, id, time created, message, like count, comment count,
+and dict of reactions
+from the post dict created in the graph API
 """
 
 def post_info(post):
@@ -58,13 +55,15 @@ def post_info(post):
     post_doc["created_time"] = post["created_time"]
     post_doc["from"] = post["from"]["name"]
     post_doc["id"] = get_post_id(post["id"])
+    post_doc["comment_count"] = post["comments"]["summary"]["total_count"]
+    post_doc["like_count"] = post["likes"]["summary"]["total_count"]
     try:
         post_doc["message"] = post["message"]
     except KeyError:
         post_doc["message"] = ""
 
     try:
-        post_doc["reactions"] = get_reactions(post["reactions"]["data"])
+        post_doc["reactions"] = parse_reactions(post["reactions"]["data"])
     except KeyError:
         post_doc["reactions"] = []
 
@@ -72,7 +71,8 @@ def post_info(post):
 
 
 """
-create a document containing comment details- the poster, id, time created, id, message, and dict of reactions
+create a document containing comment details- the user, id, time created, message, comment count ans like count
+from the post dict created in the graph API
 """
 
 def comment_info(comment):
@@ -87,7 +87,8 @@ def comment_info(comment):
     return comment_doc
 
 """
-create a document containing comment details- the poster, id, time created, id, message, and dict of reactions
+create a document containing reply details- the poster, id, time created, message, comment parent id and like count
+from the post dict created in the graph API
 """
 
 def reply_info(reply):
@@ -106,22 +107,33 @@ create a document containing the group name, description, number of members requ
 TODO: get member count from the web, as well as admins
 """
 
-def group_info(obj):
+def group_info(group):
     group_doc = {}
-    group_doc["name"] = obj["name"]
-    group_doc["id"] = obj["id"]
-    group_doc["decsription"] = obj["description"]
-    group_doc["member_request_count"] = obj["member_request_count"]
+    group_doc["name"] = group["name"]
+    group_doc["id"] = group["id"]
+    group_doc["decsription"] = group["description"]
+    group_doc["member_request_count"] = group["member_request_count"]
     return group_doc
+
+
+"""
+connects to facebook and returns a document containing the group details
+"""
 
 def get_group_description():
     feed = graph.get_object(id=GID, fields='name, description, member_request_count, feed.limit(1)')
+    pp.pprint(feed)
     return group_info(feed)
 
-def get_group_posts_no_date(since, until):
+"""
+connects to facebook and returns a list of documents containing all the posts in the given group
+"""
+
+def get_all_group_posts():
     post_docs = []
-    feed = graph.get_object(id=GID, since=since, until=until,
-                            fields='feed{created_time, from, reactions, message}')["feed"]
+    feed = graph.get_object(id=GID, fields='feed{created_time, from, reactions, message, likes.summary(true),'
+                                           'comments.summary(true)}')["feed"]
+
     # Wrap this block in a while loop so we can keep paginating requests until
     # finished.
     while (True):
@@ -137,20 +149,25 @@ def get_group_posts_no_date(since, until):
 
     return post_docs
 
-def get_group_posts(since, until):
+
+"""
+connects to facebook and returns a list of documents containing all the posts in the given group
+parameter since, until, are string representing the date in format: yyyy-mm-dd 
+"""
+
+def get_group_posts_in_range(since, until):
     post_docs = []
     feed = graph.get_object(id=GID, since=since, until=until,
-                            fields='feed{created_time, from, reactions, message}')["feed"]
+                            fields='feed{created_time, from, reactions, message, likes.summary(true),'
+                                           'comments.summary(true)}')["feed"]
+    # set cursor on given date range
+    pattern = '%Y-%m-%d'
+    since_epoch = str(int(time.mktime(time.strptime(since, pattern))))
+    until_epoch = str(int(time.mktime(time.strptime(until, pattern))))
+    url = feed['paging']['next'] + "&since=" + since_epoch + "&until=" + until_epoch
+
     # Wrap this block in a while loop so we can keep paginating requests until
     # finished.
-
-    date_since = '10.09.2016'
-    pattern = '%d.%m.%Y'
-    since1 = str(int(time.mktime(time.strptime(date_since, pattern))))
-    date_until = '29.09.2016'
-    until1 = str(int(time.mktime(time.strptime(date_until, pattern))))
-    url = feed['paging']['next'] + "&since=" + since1 + "&until=" + until1
-    print "firsturl: " + url
     while (True):
         try:
             for post in feed['data']:
@@ -159,7 +176,6 @@ def get_group_posts(since, until):
             # Attempt to make a request to the next page of data, if it exists.
             feed = requests.get(url).json()
             url = feed['paging']['next']
-            print "url: " + url
         except KeyError:
             # When there are no more pages (['paging']['next']), break from the
             # loop and end the script.
@@ -168,67 +184,53 @@ def get_group_posts(since, until):
     return post_docs
 
 
-def get_group_comment(post_id):
-    comment_docs = []
-
-    comments = graph.get_connections(id=post_id, since=SINCE, until=UNTIL,
-                                 connection_name='comments')["feed"]
-    # Wrap this block in a while loop so we can keep paginating requests until
-    # finished.
-    while (True):
-        try:
-            for comment in comments['data']:
-                comment_docs.append(comment_info(comment))
-            # Attempt to make a request to the next page of data, if it exists.
-            feed = requests.get(feed['paging']['next']).json()
-        except KeyError:
-            # When there are no more pages (['paging']['next']), break from the
-            # loop and end the script.
-            break
-
-    return comment_docs
 """
-if __name__ == '__main__':
-    #TOKEN = str(raw_input("Access token: "))
-    #GID = str(raw_input("Group ID: "))
-    # SINCE = str(raw_input("posts since (insert date in dd/mm/yyyy format):"))
-    # UNTIL = str(raw_input("posts until (insert date in dd/mm/yyyy format):"))
-
-    feed = graph.get_object(id=GID, since=SINCE, until=UNTIL,
-                            fields='name, description, member_request_count,'
-                                     'feed.limit(5){created_time, from, reactions, message}')
-
-
-    all_posts = feed['feed']['data']
-
-    for post in all_posts:
-        post_doc = post_info(post)
-        print "\n printing post in feed"
-        pp.pprint(post_doc)
-        comments = graph.get_connections(id=post_doc["id"], since=SINCE, until=UNTIL,
-                                             connection_name='comments')
-        for comment in comments["data"]:
-            comment_object = graph.get_object(id=comment["id"], since=SINCE, until=UNTIL, fields='id, comment_count, '
-                                            'from, like_count, created_time, message, parent')
-            print "\n printing post comments info for post " + post_doc["id"]
-            pp.pprint(comment_info(comment_object))
-            if (comment_info(comment_object)["comment_count"] != 0):
-                print "\n trying to print comment replies:"
-                replies_info = graph.get_object(id=comment["id"], fields='comments{comments, from, id, message, '
-                                                                         'like_count, parent, created_time}')
-                #pp.pprint(replies_info)
-                for reply in replies_info["comments"]["data"]:
-                    reply_doc = reply_info(reply)
-                    pp.pprint(reply_doc)
-
+connects to facebook and returns a list of documents containing all the comments in the given post
 """
 
-"""
-def get_members(gid):
-  #  print 'https://www.facebook.com/groups/' + gid + '/members/'
-    page = urllib2.urlopen('https://www.facebook.com/groups/' + gid)
-    print page.read()
-    #soup = BeautifulSoup(page.read())
-    #print soup.find('head')
+def get_group_comment(post):
+    if (post["comment_count"] != 0):
+        comment_docs = []
+        comments = graph.get_connections(id=post["id"], connection_name='comments')
+        # Wrap this block in a while loop so we can keep paginating requests until
+        # finished.
+        while (True):
+            try:
+                for comment in comments['data']:
+                    comment_object = graph.get_object(id=comment["id"], fields='id, comment_count, '
+                                                      'from, like_count, created_time, message, parent, '
+                                                      'comments.limit(1000)')
+                    comment_docs.append(comment_info(comment_object))
+                # Attempt to make a request to the next page of data, if it exists.
+                comments = requests.get(comments['paging']['next']).json()
+            except KeyError:
+                # When there are no more pages (['paging']['next']), break from the
+                # loop and end the script.
+                break
+
+        return comment_docs
+
 
 """
+connects to facebook and returns a list of documents containing all the replies in the given comment
+"""
+
+def get_group_reply(comment):
+    if (comment["comment_count"] != 0):
+        reply_docs = []
+        replies = graph.get_object(id=comment["id"], fields='comments{comments, from, id, message, '
+                                                                 'like_count, parent, created_time}')
+        # Wrap this block in a while loop so we can keep paginating requests until
+        # finished.
+        while (True):
+            try:
+                for reply in replies["comments"]["data"]:
+                    reply_docs.append(reply_info(reply))
+                # Attempt to make a request to the next page of data, if it exists.
+                replies = requests.get(replies['paging']['next']).json()
+            except KeyError:
+                # When there are no more pages (['paging']['next']), break from the
+                # loop and end the script.
+                break
+
+        return reply_docs
--- uploader.py
+++ uploader.py
@@ -1,7 +1,6 @@
 from elasticsearch import Elasticsearch
 import collector
 import pprint
-import facebook
 f = open('es_details.txt', 'r')
 SERVER = f.readline()
 es = Elasticsearch([{'host': SERVER, 'port': 443, 'use_ssl': True}])
@@ -12,34 +11,26 @@ pp = pprint.PrettyPrinter(indent=4)
 #check elasticsearch is responding
 print es.info()
 
-f2 = open('facebook_details.txt', 'r')
-
-TOKEN = f2.readline()
-GID = f2.readline()
-graph = facebook.GraphAPI(TOKEN)
-post_docs=[]
-#feed = graph.get_object(id=GID, since='1486935260', until='1489354460', fields='feed{created_time, from, reactions, message}')["feed"]
-# Wrap this block in a while loop so we can keep paginating requests until
-# finished.
-#for post in feed['data']:
-#    post_docs.append(collector.post_info(post))
-
-#print len(post_docs)
-#pprint.pprint(feed)
 #upload posts
 
 #description_doc = collector.get_group_description()
 
-posts_doc_list = collector.get_group_posts("2017-01-20","2017-01-22")
-print len(posts_doc_list)
+# all_posts_doc_list = collector.get_all_group_posts()
 
-for post in posts_doc_list:
-    pp.pprint(post)
+posts_doc_list = collector.get_group_posts_in_range("2017-01-01","2017-01-2")
 
-posts_doc_list = collector.get_group_posts_no_date("2017-01-20","2017-01-22")
-print len(posts_doc_list)
-print "first docs with no date:"
-pp.pprint (posts_doc_list[:10])
-print "lasts docs with no date::"
-pp.pprint(posts_doc_list[-9:])
+pp.pprint(posts_doc_list[:-5])
+
+print "working on some comments: "
+
+for post in posts_doc_list[:5]:
+    print "comments for post " + post["id"]
+    com = collector.get_group_comment(post)
+    pp.pprint(com)
+    if com is not None:
+        for comment in com:
+            pp.pprint(collector.get_group_reply(comment))
+
+
+
 
